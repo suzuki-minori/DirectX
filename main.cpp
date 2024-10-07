74d461ced6824b9827405a0125e252d35a3d1507
ni4nn <146914397+ni4nn@users.noreply.github.com>
2024-10-07 15:58:43 +0900

    inputDevice追加

--- main.cpp
+++ main.cpp
@@ -21,11 +21,15 @@
 #include<fstream>
 #include<sstream>
 #include<wrl.h>
+#define DIRECTINPUT_VERSION 0x0800
+#include<dinput.h>
 
 #pragma comment(lib,"d3d12.lib")
 #pragma comment(lib,"dxgi.lib")
 #pragma comment(lib,"dxguid.lib")
 #pragma comment(lib,"dxcompiler.lib")
+#pragma comment(lib,"dinput8.lib")
+#pragma comment(lib,"dxguid.lib")
 
 #include"externals/imgui/imgui.h"
 #include"externals/imgui/imgui_impl_dx12.h"
@@ -609,6 +613,26 @@ D3DLeakChecker leakChecker;
 	//デバイスの生成がうまくいかなかったので起動できない
 	assert(device != nullptr);
 	Log("Complete create D3D12device!!!\n");
+
+
+
+
+	IDirectInput8* directInput = nullptr;
+	hr = DirectInput8Create(wc.hInstance, DIRECTINPUT_VERSION, IID_IDirectInput8, (void**)&directInput, nullptr);
+	assert(SUCCEEDED(result));
+	IDirectInputDevice8* keyboard = nullptr;
+	hr = directInput->CreateDevice(GUID_SysKeyboard, &keyboard, NULL);
+	assert(SUCCEEDED(hr));
+	hr = keyboard->SetDataFormat(&c_dfDIKeyboard);
+	assert(SUCCEEDED(hr));
+	hr = keyboard->SetCooperativeLevel(hwnd, DISCL_FOREGROUND | DISCL_NONEXCLUSIVE | DISCL_NOWINKEY);
+	assert(SUCCEEDED(hr));
+
+
+
+
+
+
 
 	//
 #ifdef _DEBUG
@@ -1387,6 +1411,17 @@ D3DLeakChecker leakChecker;
 			materialDataSprite->uvTransform = uvTransformMatrix;
 
 			
+			//
+			keyboard->Acquire();
+			//
+			BYTE key[256] = {};
+			keyboard->GetDeviceState(sizeof(key), key);
+			
+			/*if (key[DIK_0]) {
+				OutputDebugStringA("hit 0\n");
+			}*/
+
+
 			
 			ImGui::Begin("texture");
 			ImGui::DragFloat3("*scale", &transform.scale.x);//InputFloatだと直入力のみ有効
